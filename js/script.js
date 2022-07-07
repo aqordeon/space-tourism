aeae06ee94b9e46f217a2c447c489d0aa1d621cf
AqordeoN <aqordivika@yahoo.co.id>
2022-07-08 05:17:22 +0700

    completing animation for Crew & Technology's page

--- js/script.js
+++ js/script.js
@@ -34,21 +34,6 @@ $('.navbar-destination').on('click', function() {
 })
 
 
-// TECHNOLOGY
-$('.technology-number').on('click', function() {
-    $('.technology-number').removeClass('technology-active');
-    $(this).addClass('technology-active');
-
-    let technologyNumber = $(this).html()
-
-    $.getJSON('data.json', function(data) {
-        let technologies = data.technology[technologyNumber - 1];
-        $('#content-name').html(technologies.name);
-        $('#content-desc').html(technologies.description);
-        document.getElementById("technology-image").src = `${technologies.images.portrait}`;
-    })
-})
-
 // CREW
 $('.crew-bullets li').on('click', function() {
     $('.crew-bullets li').removeClass('crew-active');
@@ -60,15 +45,55 @@ $('.crew-bullets li').on('click', function() {
         const containList = $(list[index]).hasClass("crew-active")
         if (containList == true) {
             $.getJSON('data.json', function(data) {
-                $('#title-crew').html(data.crew[index].role);
-                $('#content-name').html(data.crew[index].name);
-                $('#content-desc').html(data.crew[index].bio);
-                document.getElementById("crew-image").src = `${data.crew[index].images.webp}`;
+                // $('#title-crew').html(data.crew[index].role);
+                // $('#content-name').html(data.crew[index].name);
+                // $('#content-desc').html(data.crew[index].bio);
+                // document.getElementById("crew-image").src = `${data.crew[index].images.webp}`;
+
+                $('#title-crew').remove();
+                $('#content-name').remove();
+                $('#content-desc').remove();
+                $('#crew-image').remove();
+
+                $('.content-main').prepend(`
+                <div class="wrapper-swipe-up"><h5 class="title-crew swipe-up" id="title-crew">${data.crew[index].role}</h5></div>
+                <div class="wrapper-swipe-up"><h1 class="content-name swipe-up-high" id="content-name">${data.crew[index].name}</h1></div>
+                <div class="wrapper-swipe-up"><p class="content-desc swipe-up-high  " id="content-desc">${data.crew[index].bio}</p></div>`);
+                $('.crew-right').append(`<img src="${data.crew[index].images.webp}" alt="" srcset="" id="crew-image" class="crew-image reveal">`);
             })
         }
     });
+})
 
 
+// TECHNOLOGY
+$('.technology-number').on('click', function() {
+    $('.technology-number').removeClass('technology-active');
+    $(this).addClass('technology-active');
 
-
+    let technologyNumber = $(this).html()
+    $.getJSON('data.json', function(data) {
+            let technologies = data.technology[technologyNumber - 1];
+            $('.content-main > .slide').remove();
+            $('.content-main').append(`
+            <div class="slide">
+                <h1 class="content-name" id="content-name">${technologies.name}</h1>
+            </div>
+            <div class="slide animdelay4">
+                <p class="content-desc" id="content-desc">${technologies.description}</p>
+            </div>
+            `);
+            $('.technology-image').remove();
+            $('.content-right').append(`
+                <div class="technology-image slide-reverse animdelay2">
+                    <img src="${technologies.images.portrait}" alt="" srcset="" id="technology-image">
+                </div>
+            `);
+            // document.getElementById("technology-image").src = `${technologies.images.portrait}`;
+        })
+        // $.getJSON('data.json', function(data) {
+        //     let technologies = data.technology[technologyNumber - 1];
+        //     $('#content-name').html(technologies.name);
+        //     $('#content-desc').html(technologies.description);
+        // })
 })
